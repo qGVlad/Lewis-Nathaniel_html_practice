cd96ec8ec6ac68761f59c0e89c0e540cbd707c1b
DrVladislav <vlad.gudkov.99@gmail.com>
2020-04-01 16:28:53 +0300

    Show modals on click

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,6 @@
 $(function() {
+  // ===== Filter =====
   const filter = $("[data-filter]");
-
   filter.on("click", function(e) {
     e.preventDefault();
 
@@ -19,4 +19,37 @@ $(function() {
       $("[data-category]").removeClass("hide");
     }
   });
+
+  // ===== Show modal and close if click was to background =====
+  const modal = $("[data-modal]");
+  modal.on("click", function(e) {
+    e.preventDefault();
+
+    const modalId = $(this).data("modal");
+    $(modalId).addClass("show");
+    removeScrollBody();
+  });
+  $(".modal").on("click", function(e) {
+    $(this).removeClass("show");
+    addScrollBody();
+  });
+  $(".modal__dialog").on("click", function(e) {
+    e.stopPropagation();
+  });
+
+  // ===== Click on close btn =====
+  const modalClose = $("[data-close]");
+  modalClose.on("click", function(e) {
+    let parent = modalClose.parents(".modal.show");
+    parent.removeClass("show");
+    addScrollBody();
+  });
+
+  // ===== Helpers =====
+  function removeScrollBody() {
+    $("body").addClass("no-scroll");
+  }
+  function addScrollBody() {
+    $("body").removeClass("no-scroll");
+  }
 });
